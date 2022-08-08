ca02df6486cd5ba17098b0718795bd67f9d06674
Karan Pal Singh <kdsgyt@gmail.com>
2022-08-08 11:02:17 -0400

    Finished the basic setup. The hero is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeroComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
