26ff7845e1c07fdb22478233dc07379149e9111e
Karan Pal Singh <kdsgyt@gmail.com>
2022-08-27 11:26:01 -0400

    fixed the color issue for try for free button

--- src/app/components/try-for-free-button/try-for-free-button.component.ts
+++ src/app/components/try-for-free-button/try-for-free-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GLOBALpurple } from 'src/assets/variables';
 
 @Component({
   selector: '[app-try-for-free-button],app-try-for-free-button ',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./try-for-free-button.component.scss']
 })
 export class TryForFreeButtonComponent implements OnInit {
+  @Input() backgroundColor: string = 'white';
+  // buttonColor: string = this.backgroundColor!=='white' ? 'white': GLOBALpurple;
+  @Input() buttonColor: string = GLOBALpurple;
+  
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { GLOBALpurple } from 'src/assets/variables';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +9,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   public visible: Boolean = false;
-  @Input() sticky: Boolean = false;
-  // private scrollOffset:
-  constructor() { }
+  @Input() public sticky: Boolean = false;
+  private thresholdOffsetValue: number = 400;
+  public talkToSalesButtonColor: string = GLOBALpurple;
+  // public tryForFreeButtonColor: string = 
+  constructor() {
+
+  }
+
+  // This cahnges the state to visible or not visible when the users scrolls the page.
+
+  /**
+   * Functioning: when the user scrolls the page th pageoffsetvalue is changes. ( a global varaible.) 
+   * we use this event listener to keep an eye on the scroll offsetvalue and when the value is more 
+   * than a certain value the navbar becomes visible otherwise stays hidden.
+   */
+  @HostListener('window:scroll')
+  private handleScroll() {
+    if (window.pageYOffset >= this.thresholdOffsetValue) {
+      this.visible = true;
+    } else if (window.pageYOffset < this.thresholdOffsetValue) {
+      this.visible = false;
+    }
+  }
 
   ngOnInit(): void {
-    
+
   }
 
 }
