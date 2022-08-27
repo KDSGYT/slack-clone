edccfb4cbeaec49b6cb1f67e8dc1f6e010ef05a9
Karan Pal Singh <kdsgyt@gmail.com>
2022-08-26 23:38:06 -0400

    Navbar is sticky now. Working on the animation to play when the user scrolls the page.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss', './floatingNavbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
+  public visible: Boolean = false;
+  @Input() sticky: Boolean = false;
+  // private scrollOffset:
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
